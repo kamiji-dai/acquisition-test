de8f23979fa723a024523ef9efb8129e9df45b99
kamiji-dai <kamday723@gmail.com>
2024-12-03 12:53:47 +0900

    Cookie表示をリアクティブ化

--- app/complete/page.tsx
+++ app/complete/page.tsx
@@ -1,8 +1,16 @@
+import Script from 'next/script';
+
 export default function Complete() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-xl shadow-md">
-        <p className="text-xl text-gray-800">申し込みが完了しました。</p>
+    <div>
+      <Script
+        src="https://strage-dev.meet-meet.com/static/script/trackConversion.js?mud=100"
+        strategy="lazyOnload"
+      />
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white p-8 rounded-xl shadow-md">
+          <p className="text-xl text-gray-800">申し込みが完了しました。</p>
+        </div>
       </div>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,44 @@
 "use client"
 
+import { useEffect, useState } from 'react';
 import Script from 'next/script';
 import Link from 'next/link'
 import Button from '@/components/Button'
 
+function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift();
+  return null;
+}
+
+function useCookie(name: string) {
+  const [cookieValue, setCookieValue] = useState<string | null>(null);
+
+  useEffect(() => {
+    // クッキーの値が変更されたら更新
+    const checkCookie = () => {
+      const newValue = getCookie(name);
+      if (newValue !== cookieValue) {
+        setCookieValue(newValue ?? null);
+      }
+    };
+
+    checkCookie(); // 初期値を設定
+
+    const intervalId = setInterval(checkCookie, 2000); // 2秒ごとにチェック
+    return () => {
+      // インターバルIDをクリア
+      clearInterval(intervalId);
+    };
+  }, [name, cookieValue]);
+
+  return cookieValue;
+}
+
 export default function Home() {
-    // クッキーの値を取得する関数
-  function getCookie(name: string) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(';').shift();
-  }
-
-  const mcvsnValue = getCookie('mcvsn');
-
+  const mcvsnValue = useCookie('mcvsn');
+  
   return (
     <div>
       <Script
